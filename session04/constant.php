a9d0f553454dedaa0300e8e9603119054bdee84c
mshahmalaki <mshahmalaki@gmail.com>
2015-12-17 07:06:09 -0800

    constant.php File

---
+++ session04/constant.php
@@ -0,0 +1,6 @@
+<?php 
+define('DS', 'DIRECTORY_SEPARATOR');
+define('PS', 'PATH_SEPARATOR');
+define('BASE_DIR', '__DIR__');
+define('WEBROOT', BASE_DIR.DS.'webroot');
+define('ADMIN_URL', BASE_URL.'/admin/');   //BASE_URL Constant Defined in config.php File
